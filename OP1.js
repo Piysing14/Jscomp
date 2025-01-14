a8e5242b4b43258273a18262f1eadd85cc01c3df
Piyush Singh <khatipk14@gmail.com>
2025-01-15 03:24:55 +0530

    adding output questions

---
+++ OP1.js
@@ -0,0 +1,66 @@
+// // var x = 0;
+// // function foo() {
+// // x++;
+// // if (x > 3) {
+// // clearTimeout(timerId);
+// // }
+// // }
+// // var timerId = setTimeout(function() {
+// // console.log('Hello');
+// // }, 1000);
+// // setInterval(foo, 1000);
+// // function myFunction(x, y) {
+// //     const arr = Array.prototype.slice.call(arguments);
+// //     return arr.length;
+// //     }
+
+// //     console.log(myFunction(5))
+
+// // let str = 'hello';
+// // let newStr = str.replace('l', 'L');
+// // console.log(newStr);
+
+// // var x = 0;
+// // function foo() {
+// // x++;
+// // console.log(x);
+// // }
+// // var timerId = setInterval(foo, 1000);
+// // setTimeout(function() {
+// // clearInterval(timerId);
+// // }, 5000);
+
+// // const obj = {
+// //     a: 1,
+// //     b: 2
+// //     };
+// //     const arr = [];
+// //     console.log(arr[0] = { ...obj });
+
+
+// // console.log(5&9)
+
+// let x = 5;
+// x += 2;
+
+// let person = {
+//     name: "John",
+//     age: 30,
+//     get fullName() {
+//     return `${this.name} Smith`;
+//     },
+//     set fullName(value) {
+//     [this.name, this.surname] = value.split(" ");
+//     }
+//     };
+//     person.fullName = "Jane Doe";
+//     console.log(person.name);
+
+//     let str = 'hello';
+// let newStr = str.replace('l', 'L');
+// console.log(newStr);
+
+function a(){
+
+}
+console.log(typeof(a))
