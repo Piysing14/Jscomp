d5e2631a80665f4cebed5bd4e3ce8194bb290731
Piyush Singh <khatipk14@gmail.com>
2025-01-18 00:52:09 +0530

    .

--- regEx.js
+++ regEx.js
@@ -2,5 +2,16 @@ function isValidURL(url) {
     const regex = /^(ftp|http|https):\/\/[^\s]+$/;
      return regex.test(url);
   }
-  
   console.log(isValidURL('https://www.google.com'))
+
+
+  //Extract number from a string
+
+  function extractNumbers(str) {
+    // Define regex to match integers and floating-point numbers
+    const regex = /\d+(\.\d+)?/g;
+    
+    // Match numbers and convert matched strings to numbers, or return an empty array if no matches
+    const matches = str.match(regex);
+    return matches ? matches.map(Number) : [];
+}
